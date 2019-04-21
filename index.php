e57ae835ece0c7b849316e552275481bee3d28f0
phamvanlonghn <phamvanlonghn@gmail.com>
2019-04-21 10:29:28 +0700

    Update file front-end

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body class="home index">
+    <div class="wrap">
+        <nav class="nav nav-custom">
+            <h1>LOGIN</h1>
+            <form action="" class="formsignin">
+                <input type="text" placeholder="username/email" name="user_uid">
+                <input type="password" placeholder="password" name="user_pwd">
+                <input type="submit" value="Login">
+            </form>
+
+            <a href="signup.php" class="btn btn-red signup">Register?</a>
+        </nav>
+
+        <section>
+            <h3>Home</h3>
+        </section>
+
+        <footer>
+            <p>Design by</p>
+        </footer>
+    </div>
+</body>
+</html>
